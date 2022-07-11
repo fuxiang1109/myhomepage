890aa16f557962fdd98214f83e2bcd8b3a3f5f3c
Sun Fuxiang <108178105+fuxiang1109@users.noreply.github.com>
2022-07-11 15:22:36 +0800

    Delete lxd directory

--- lxd/js/index.js
+++
@@ -1,8 +0,0 @@
-$("#login-button").click(function (event) {
-  event.preventDefault();
-  $("form").fadeOut(500);
-  $(".wrapper").addClass("form-success");
-  setTimeout(function () {
-    location.href = "BirthdayCake.html";
-  }, 2000);
-});
